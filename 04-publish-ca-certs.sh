85f7d57aa274e8e1911e1b562242b76819764511
Mikhail Pridushchenko <mikhail.pridushchenko@dsr-corporation.com>
2018-12-14 17:31:03 +0300

    Implement CA certs publishing for Debian

--- 04-publish-ca-certs.sh
+++ 04-publish-ca-certs.sh
@@ -9,4 +9,8 @@ if [[ "$KERNEL_RELEASE" =~ "ARCH" ]]
 then
 	cat root-ca.crt sub-ca.crt > /etc/ca-certificates/trust-source/anchors/pdk_vpn.pem
 	trust extract-compat
+elif $(which update-ca-certificates &> /dev/null)
+then
+	cp root-ca.crt sub-ca.crt /usr/local/share/ca-certificates/
+	update-ca-certificates
 fi
